ecf899fe1f9b80ed2bb9f7ae349db67ada362ac4
Dang Nguyen <nguyenhaidang19931015@gmail.com>
2020-12-08 11:01:44 +0700

    color box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to Reactjs</h1>
-        <h2>Dang Nguyen</h2>
+      <h2>Dang Nguyen</h2>
+      <ColorBox />
     </div>
   );
 }
---
+++ src/components/ColorBox/index.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import '../../App.scss';
+
+const getRandomColorBox = () => {
+    const listColor = ['deeppink', 'green', 'yellow', 'black', 'blue']
+    const colorPick = Math.trunc(Math.random() * 5)
+    return listColor[colorPick]
+}
+
+const ColorBox = () => {
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink'
+        return initColor
+    });
+    const handleClickColorBox = () => {
+        const colorBox = getRandomColorBox()
+        setColor(colorBox)
+        localStorage.setItem('box_color', colorBox)
+    }
+
+    return (
+    <div
+        className="color-box"
+        style={{backgroundColor: color}}
+        onClick={handleClickColorBox}
+        >
+        
+    </div>);
+}
+
+export default ColorBox;
